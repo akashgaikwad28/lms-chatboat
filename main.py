502258652fabd24ad6546c54732512c59845b0fd
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2025-07-26 20:50:47 +0530

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get("/")
+# def read_root():
+#     return {"message": "Hello Radhyeee I Love You"}
+
+
+# ??????????????????????????????????????????????????
+
+
+# main.py
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from agents.lms_agent import run_agent
+from config.settings import settings
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello Radhyeee I Love You"}
+class ChatRequest(BaseModel):
+    query: str
+    user_id: str = None
+
+@app.post("/chat")
+async def chat_with_bot(req: ChatRequest):
+    response = await run_agent(req.query, req.user_id)
+    return {"response": response}
