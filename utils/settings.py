e28b769a6f78785fa011bae3d9de17de8da14ed7
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2025-07-26 21:49:56 +0530

    setting updation

--- utils/settings.py
+++ utils/settings.py
@@ -1,8 +1,9 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from config.settings import settings
 
-def get_llm():
-    return ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
-        google_api_key=settings.GOOGLE_API_KEY
-    )
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    GOOGLE_API_KEY: str
+
+settings = Settings()
