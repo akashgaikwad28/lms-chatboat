bafed94d20abd65005514cb5ecfa140c409da7c3
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2025-07-29 19:01:54 +0530

    token limit added

--- utils/llm_provider.py
+++ utils/llm_provider.py
@@ -10,5 +10,7 @@ print("Using Google API key:", settings.GOOGLE_API_KEY)
 def get_llm():
     return ChatGoogleGenerativeAI(
         model="gemini-pro",
-        google_api_key=settings.GOOGLE_API_KEY  
+        google_api_key=settings.GOOGLE_API_KEY,
+        temperature=0.1,
+        max_output_tokens=512,
     )
