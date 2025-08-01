bf42002e8a1ec4a17d1a8b1230fbe64e48679459
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2025-08-01 21:32:39 +0530

    groq configuration

--- config/settings.py
+++ config/settings.py
@@ -2,8 +2,17 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
 
+# class Settings(BaseSettings):
+#     GOOGLE_API_KEY: str = Field(..., alias="GOOGLE_API_KEY")
+#     LMS_API_BASE_URL: str = Field(..., alias="LMS_API_BASE_URL")
+
+#     class Config:
+#         env_file = ".env"
+#         populate_by_name = True
+
+
 class Settings(BaseSettings):
-    GOOGLE_API_KEY: str = Field(..., alias="GOOGLE_API_KEY")
+    GROQ_API_KEY: str = Field(..., alias="GROQ_API_KEY")
     LMS_API_BASE_URL: str = Field(..., alias="LMS_API_BASE_URL")
 
     class Config:
