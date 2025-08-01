55c947682cf18fbeef8b6d0538b239c5a84f14d3
Akash Gaikwad <akash.gaikwad9945@gmail.com>
2025-08-01 21:36:30 +0530

    Groq llm provider

--- utils/llm_provider.py
+++ utils/llm_provider.py
@@ -1,16 +1,28 @@
 # utils/llm_provider.py
 
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
+
+from langchain_groq import ChatGroq
 
 from config.settings import settings
 
 
-print("Using Google API key:", settings.GOOGLE_API_KEY)
+# print("Using Google API key:", settings.GOOGLE_API_KEY)
+
+# def get_llm():
+#     return ChatGoogleGenerativeAI(
+#         model="gemini-pro",
+#         google_api_key=settings.GOOGLE_API_KEY,
+#         temperature=0.1,
+#         max_output_tokens=512,
+#     )
+
+
 
 def get_llm():
-    return ChatGoogleGenerativeAI(
-        model="gemini-pro",
-        google_api_key=settings.GOOGLE_API_KEY,
-        temperature=0.1,
-        max_output_tokens=512,
+    return ChatGroq(
+        groq_api_key=settings.GROQ_API_KEY,
+        model_name="llama3-70b-8192" ,
+        temperature=0.01,
+        max_tokens=512  
     )
